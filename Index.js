b10e6a11188462bf353839c3261027e6cf473006
jugomarcos@hotmail.como <marcosjugo@hotmail.com>
2023-03-08 11:46:16 -0300

    Escribe en el display, sigue sin corregirse que se puedan escribir seguido

---
+++ Index.js
@@ -0,0 +1,53 @@
+class Calculadora {
+    constructor(operadorDataAnteriorTexto, operadorDataActualTexto) {
+        this.operadorDataAnteriorTexto = operadorDataAnteriorTexto;
+        this.operadorDataActualTexto = operadorDataActualTexto;
+        console.clear();
+    }
+
+    limpiarAll(){
+        this.operadorActual =' ';
+        this.operadorAnterior ='';
+        this.operacion = undefined;
+    }
+
+    borrarSimple(){
+    }
+
+    elegirNumeros(numero){
+        this.operadorActual = '';
+        this.operadorActual = this.operadorActual.toString() + numero.toString();
+    }
+
+    igual(){
+
+    }
+
+    elegirOperaciones(){
+
+    }
+
+    actualizarPantalla(){
+        this.operadorDataActualTexto.innerText = this.operadorActual
+
+    }
+  
+}
+
+const operadorDataAnteriorTexto = document.querySelector('[data-anterior-operador]');
+const operadorDataActualTexto = document.querySelector('[data-actual-operador]');
+const botonesNumeros = document.querySelectorAll('[data-numero]');
+const botonesOperadores = document.querySelectorAll('[data-operacion]');
+const botonIgual = document.querySelector('[data-igual]');
+const botonBorrarTodo = document.querySelector(['data-borrar-todo']);
+const botonBorrarSimple = document.querySelector(['data-borrar-simple']);
+
+const calculadora = new Calculadora (operadorDataAnteriorTexto, operadorDataActualTexto)
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () =>{
+        calculadora.elegirNumeros(boton.innerText)
+        calculadora.actualizarPantalla()
+    })
+});
+
